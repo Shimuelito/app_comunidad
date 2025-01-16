c943a723a0a0563e7eda40e88f175db040872f63
CETECOM <cetecom_conce@duoc.cl>
2025-01-16 10:29:11 -0300

    se valido el registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router'; 
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -10,7 +11,9 @@ import { Router } from '@angular/router';
 })
 export class RegistroPage implements OnInit {
 
-  constructor(private router: Router) {}
+  registroForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder ,private router: Router) {}
   isAlertOpen = false;
   alertButtons = [
     {
@@ -30,6 +33,27 @@ export class RegistroPage implements OnInit {
   
 
   ngOnInit() {
+     // Inicializa el formulario con validaciones
+     this.registroForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      numeroCasa: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    }, { validator: this.passwordMatchValidator });
+  }
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+  onSubmit() {
+    if (this.registroForm.valid) {
+      console.log('Formulario válido:', this.registroForm.value);
+      this.router.navigate(['/home']); // Redirige a la página principal
+    } else {
+      console.log('Formulario inválido');
+    }
   }
 
 }
